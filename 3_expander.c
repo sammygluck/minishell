88895b1e8f9c10b4d13ce9efade50f699e332beb
sgluck <infogls@yahoo.com>
2023-11-12 13:11:07 +0100

    started thinking about expander

--- 3_expander.c
+++ 3_expander.c
@@ -1,2 +1,19 @@
 #include <minishell.h>
 
+//create a system for the $ sign to be translated to the actual text
+//remove quotes and where necessary interpret the $ sign
+//handle heredoc quotes
+    //remove but flag a quoted heredoc
+//also some hardcode stuff for the $ sign
+
+//lexer_tokens -> expanded_lexer_tokens
+/*
+$ = $
+$? = exit code
+$VAR = the value in the var
+$NO_EXIST = empty
+$'string' = string
+$'$hello' = echo the string '$hello'
+$"$TRUE_VAR" = var
+
+*/
