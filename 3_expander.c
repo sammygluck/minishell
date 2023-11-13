ae175e9b9a2be7fa7279fde2a39193909ac059f4
Sammy Gluck <infogls@yahoo.com>
2023-11-13 16:04:37 +0100

    dollar before string remover

--- 3_expander.c
+++ 3_expander.c
@@ -6,14 +6,43 @@
     //remove but flag a quoted heredoc
 //also some hardcode stuff for the $ sign
 
-//lexer_tokens -> expanded_lexer_tokens
-/*
-$ = $
-$? = exit code
-$VAR = the value in the var
-$NO_EXIST = empty
-$'string' = string
-$'$hello' = echo the string '$hello'
-$"$TRUE_VAR" = var
+//a lot of string manipulation ahead!!!
 
-*/
+
+//1st manipulation
+//extract the strings from $"string"
+//if $-sign is followed by a quote
+//find second quote and remove dollar sign
+t_token *expander(t_token **token_head, t_env_var *var_head)
+{
+    
+    //when reassigning, make sure to free all old ones
+    //for each token->name ; two passes
+    
+}
+
+char *initial_clean(char *string)
+{
+    int i;
+    int j;
+    int in_quote;
+    char *new_string;
+
+    i = 0;
+    j = 0;
+    in_quote = 0;
+    new_string = ft_malloc(ft_strlen(string) + 1);
+    while (string[i])
+    {
+        if (is_quote(string[i]) && in_quote == 0)
+            in_quote = is_quote(string[i]);
+        else if (is_quote(string[i]) && in_quote > 0 && is_quote(string[i]) == in_quote)
+            in_quote = 0;
+        if (string[i] == '$' && is_quote(string[i + 1]) && !in_quote)
+            i++;
+        new_string[j++] = string[i];
+            i++;
+    }
+    new_string[j] = '\0';
+    return (new_string);
+}
