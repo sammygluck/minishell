37666f7b7873f74eb0f0ee98fbeef4d09b143adb
sgluck <infogls@yahoo.com>
2023-11-19 12:42:44 +0100

    expander third clean

--- 3d_expander_quote_replace.c
+++ 3d_expander_quote_replace.c
@@ -1,5 +1,6 @@
 #include <minishell.h>
 
+
 /*
 Outline quote remover
 */
@@ -8,8 +9,34 @@ Outline quote remover
 STEP 1: outline
 
 while string
-    find first quote
-    find second quote
-    remove quotes
-//make sure not to begin from the beginning in the next iteration
+    copy all characters except those that are quotes not in quoted parts
 */
+
+
+
+
+
+char *third_clean(char *string)
+{
+    t_quote q_struct;
+    char *output;
+    int i;
+    int j;
+    
+    init_quote(&q_struct);
+    output = ft_malloc(ft_strlen(string) + 1);
+    j = 0;
+    i = 0;
+    while (string[i])
+    {
+        is_in_quote(string[i], &q_struct);
+        if (is_quote(string[i]) && !q_struct.in_quote)
+        {
+            i++;
+            continue;
+        }
+        output[j++] = string[i++];        
+    }
+    output[j] = '\0';
+    return (output);
+}
