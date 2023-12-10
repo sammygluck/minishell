27a34939afe5e7b2aa815c01c97067a9bbfc82c0
Sammy Gluck <infogls@yahoo.com>
2023-12-10 14:08:08 +0100

    fixed exit_utils.c

--- 6g_builtins_exit_utils.c
+++ 6g_builtins_exit_utils.c
@@ -26,22 +26,32 @@ void process_sign_and_whitespace(char **str, int *sign)
     }
 }
 
-int convert_to_number(char *str, long long *number, int sign) {
+int convert_to_number(char *str, long long *number, int sign) 
+{
+    long long max_val;
+    int digit;
+
     *number = 0;
+    if (sign == 1)
+        max_val = LLONG_MAX;
+    else
+        max_val = -(LLONG_MIN + 1);
     while (*str) 
     {
         if (!ft_isdigit(*str))
             return (1); // Indicates invalid character
-        if (*number > LLONG_MAX / 10 || (*number == LLONG_MAX / 10 && *str - '0' > LLONG_MAX % 10)) 
+        digit = *str - '0';
+        if (*number > max_val / 10 || (*number == max_val / 10 && digit > max_val % 10)) 
             return (1); // Indicates overflow
-        *number = *number * 10 + (*str - '0');
+        *number = *number * 10 + digit;
         str++;
     }
     *number *= sign;
     return (0); // Indicates no overflow
 }
 
-long long str_to_longlong_with_overflow_check(char *str, int *overflow) {
+long long str_to_longlong_with_overflow_check(char *str, int *overflow) 
+{
     long long number;
     int sign;
 
@@ -58,7 +68,8 @@ long truncate_to_exit_code(long long number)
         return (number % 256);
 }
 
-int validate_and_process_exit_code(char *input_str) {
+int validate_and_process_exit_code(char *input_str) 
+{
     int overflow;
     long exit_code; 
     long long num;
