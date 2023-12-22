1e4291d71b3f599f249646d1cf6dc6bc150b780b
Jolande Steenput <jolandesteenput@Jolandes-MacBook-Pro.local>
2023-12-22 17:53:40 +0100

    extra functions in heredoc

--- 5g_heredoc.c
+++ 5g_heredoc.c
@@ -35,28 +35,48 @@ static void	heredoc_redirect(char *temp_file, int fd_temp, t_process *p)
 	close(p->fd_in);
 }
 
+static char	*create_heredoc_file_name()
+{
+	static int	i;
+	char		*number;
+	char		*name;
+
+	number = ft_itoa(i);
+	if (!number)
+		return (NULL);
+	name = ft_strjoin(HEREDOC_TEMP_FILE, number);
+	if (!name)
+		return (NULL);
+	free(number);
+	i++;
+	return (name);
+}
+
 void	heredoc_handler(char *delimiter, t_process *p)
 {
 	int		fd_temp;
 	char	*temp_file;
 	char	*line;
 
-	if (!delimiter)
-		error_message("please provide correct delimiter"); // TO DO: check if this is necessary
-	temp_file = "./temp/temp_file_heredoc"; // TO DO: change location of temp file
+	temp_file = create_heredoc_file_name();
+	if (!temp_file)
+		error_message("heredoc file creation error"); // TO DO: add the proper error handling?
+	if (!delimiter || *delimiter == '\0')
+		error_message("missing delimiter - syntax error near unexpected token"); // TO DO: change to correct error handler
 	fd_temp = open_file(temp_file, 3);
-	line = readline("> ");
-	if (!line)
-		error_message("heredoc input error");
-	while (line && ft_strcmp(line, delimiter) != 0)
+	while (1)
 	{
-		write(fd_temp, line, ft_strlen(line));
-		write(fd_temp, "\n", 1);
-		free(line);
 		line = readline("> ");
+		if (!line) // Q: what if *line == NULL?
+			error_message("heredoc input error"); // TO DO: change to correct error handler
+		if (ft_strcmp(line, delimiter) == 0)
+			break ;
+		ft_putendl_fd(line, fd_temp);
+		free(line);
 	}
 	free(line);
 	heredoc_redirect(temp_file, fd_temp, p);
+	// free(heredoc_file) ??
 }
 
 
