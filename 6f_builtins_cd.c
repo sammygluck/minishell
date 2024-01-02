14445489ed15ac4949fe66612498456a3e661ed0
Sammy Gluck <infogls@yahoo.com>
2024-01-02 16:29:39 +0100

    added - and -- functionality in cd

--- 6f_builtins_cd.c
+++ 6f_builtins_cd.c
@@ -31,24 +31,38 @@
 int ft_cd(char **argv, char ***env, t_env_var **env_head)
 {
     char *home;
+    char *old_pwd;
     int ret_value;
 
     ret_value = 0;
     if (!argv || !argv[0]) 
     {
-        printf("error:ft_cd\n"); // Provide more specific error message
+        ft_putstr_fd("error:ft_cd\n", 2); // Provide more specific error message
         return (1);
     }
-    if (!argv[1]) 
+    if (!argv[1] || (!ft_strncmp(argv[1], "--", 2) && ft_strlen(argv[1]) == 2))
     {
         home = get_env_value(*env_head, "HOME");
         if (!home) 
         {
-            printf("minishell: cd: HOME not set\n");
+            ft_putstr_fd("minishell: cd: HOME not set\n", 2);
             return (1);
         }
         ret_value = ft_chdir(home, env, env_head);
         free(home); // Free memory allocated by get_env_value
+        return (ret_value);
+    }
+    else if (!ft_strncmp(argv[1], "-", 1) && ft_strlen(argv[1]) == 1)
+    {
+        old_pwd = get_env_value(*env_head, "OLDPWD");
+        if (!old_pwd)
+        {
+            ft_putstr_fd("minishell: cd: OLDPWD not set\n", 2);
+            return (1);
+        }
+        ft_putendl_fd(old_pwd, 1);
+        ret_value = ft_chdir(old_pwd, env, env_head);
+        free(old_pwd);
         return (ret_value);
     }
     if (argv[2] != NULL) // Corrected argument check
@@ -84,13 +98,15 @@ int ft_chdir(const char *path, char ***env, t_env_var **env_head) //change in mi
 char *get_env_value(t_env_var *env, char *string)
 {
     t_env_var *head;
+    size_t string_len;
 
-    if (!env)
+    if (!env || !string)
         return (NULL);
+    string_len = ft_strlen(string);
     head = env;
     while (head)
     {
-        if (!strncmp(head->name, string, ft_strlen(string) + 1))
+        if (ft_strlen(head->name) == string_len && !strncmp(head->name, string, string_len))
             return (ft_strdup(head->value));
         head = head->next;
     }
