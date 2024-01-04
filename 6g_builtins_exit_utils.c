c15b21ce85c3353e279e651c8ff15c482e2cdd79
Sammy <infogls@yahoo.com>
2024-01-04 12:35:42 -0500

    major norminette overhaul

--- 6g_builtins_exit_utils.c
+++ 6g_builtins_exit_utils.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   6g_builtins_exit_utils.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sgluck <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/04 12:31:12 by sgluck            #+#    #+#             */
+/*   Updated: 2024/01/04 12:31:17 by sgluck           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
 // Assuming ft_isspace and ft_isdigit are predefined functions.
@@ -13,75 +25,75 @@ int validate_and_process_exit_code(char *input_str);
 
 // Definitions of the functions
 
-void process_sign_and_whitespace(char **str, int *sign) 
+void	process_sign_and_whitespace(char **str, int *sign)
 {
-    *sign = 1;
-    while (ft_isspace(**str))
-        (*str)++;
-    if (**str == '+' || **str == '-') 
-    {
-        if (**str == '-')
-            *sign = -1;
-        (*str)++;
-    }
+	*sign = 1;
+	while (ft_isspace(**str))
+		(*str)++;
+	if (**str == '+' || **str == '-')
+	{
+		if (**str == '-')
+			*sign = -1;
+		(*str)++;
+	}
 }
 
-int convert_to_number(char *str, long long *number, int sign) 
+int	convert_to_number(char *str, long long *number, int sign)
 {
-    long long max_val;
-    int digit;
+	long long	max_val;
+	int			digit;
 
-    *number = 0;
-    if (sign == 1)
-        max_val = LLONG_MAX;
-    else
-        max_val = -(LLONG_MIN + 1);
-    while (*str) 
-    {
-        if (!ft_isdigit(*str))
-            return (1); // Indicates invalid character
-        digit = *str - '0';
-        if (*number > max_val / 10 || (*number == max_val / 10 && digit > max_val % 10)) 
-            return (1); // Indicates overflow
-        *number = *number * 10 + digit;
-        str++;
-    }
-    *number *= sign;
-    return (0); // Indicates no overflow
+	*number = 0;
+	if (sign == 1)
+		max_val = LLONG_MAX;
+	else
+		max_val = -(LLONG_MIN + 1);
+	while (*str)
+	{
+		if (!ft_isdigit(*str))
+			return (1); // Indicates invalid character
+		digit = *str - '0';
+		if (*number > max_val / 10
+			|| (*number == max_val / 10 && digit > max_val % 10))
+			return (1); // Indicates overflow
+		*number = *number * 10 + digit;
+		str++;
+	}
+	*number *= sign;
+	return (0); // Indicates no overflow
 }
 
-long long str_to_longlong_with_overflow_check(char *str, int *overflow) 
+long long	str_to_longlong_with_overflow_check(char *str, int *overflow)
 {
-    long long number;
-    int sign;
+	long long	number;
+	int			sign;
 
-    process_sign_and_whitespace(&str, &sign);
-    *overflow = convert_to_number(str, &number, sign);
-    return (number);
+	process_sign_and_whitespace(&str, &sign);
+	*overflow = convert_to_number(str, &number, sign);
+	return (number);
 }
 
-long truncate_to_exit_code(long long number) 
+long	truncate_to_exit_code(long long number)
 {
-    if (number < 0) 
-        return (256 + number % 256);
-    else 
-        return (number % 256);
+	if (number < 0)
+		return (256 + number % 256);
+	else
+		return (number % 256);
 }
 
-int validate_and_process_exit_code(char *input_str) 
+int	validate_and_process_exit_code(char *input_str)
 {
-    int overflow;
-    long exit_code; 
-    long long num;
-    
-    overflow = 0;
-    num = str_to_longlong_with_overflow_check(input_str, &overflow);
-    if (overflow)
-        return (-1); // Handle overflow
-    else 
-    {
-        exit_code = truncate_to_exit_code(num);
-        return (exit_code); // Use the exit_code as needed
-    }
-}
+	long long	num;
+	long		exit_code;
+	int			overflow;
 
+	overflow = 0;
+	num = str_to_longlong_with_overflow_check(input_str, &overflow);
+	if (overflow)
+		return (-1); // Handle overflow
+	else
+	{
+		exit_code = truncate_to_exit_code(num);
+		return (exit_code); // Use the exit_code as needed
+	}
+}
--- 8_shlvl_export.c
+++ 8_shlvl_export.c
@@ -1,43 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   8_shlvl_export.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sgluck <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/04 12:35:17 by sgluck            #+#    #+#             */
+/*   Updated: 2024/01/04 12:35:20 by sgluck           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-void shlvl_export(char ***env, t_env_var **env_l)
+void	shlvl_export(char ***env, t_env_var **env_l)
 {
-    char *argv[3];
-    char *shell_level;
-    char *shell_argument;
-    int level_to_int;
+	char	*argv[3];
+	char	*shell_level;
+	char	*shell_argument;
+	int		level_to_int;
 
-    shell_level = return_env_value("SHLVL", *env_l); //make sure to free;
-    if (!shell_level)
-        shell_level = ft_strdup("0");
-    level_to_int = ft_atoi(shell_level);
-    level_to_int++;
-    free(shell_level);
-    shell_level = ft_itoa(level_to_int);
-    shell_argument = ft_strjoin("SHLVL=", shell_level);
-    argv[0] = "export";
-    argv[1] = shell_argument;
-    argv[2] = NULL;
-    ft_export(argv, env, env_l);
-    free(shell_level);
-    free(shell_argument);
+	shell_level = return_env_value("SHLVL", *env_l); //make sure to free;
+	if (!shell_level)
+		shell_level = ft_strdup("0");
+	level_to_int = ft_atoi(shell_level);
+	level_to_int++;
+	free(shell_level);
+	shell_level = ft_itoa(level_to_int);
+	shell_argument = ft_strjoin("SHLVL=", shell_level);
+	argv[0] = "export";
+	argv[1] = shell_argument;
+	argv[2] = NULL;
+	ft_export(argv, env, env_l);
+	free(shell_level);
+	free(shell_argument);
 }
 
 //see cd for a double of this function, remove one of the two
-char *return_env_value(char *string, t_env_var *env_l)
+char	*return_env_value(char *string, t_env_var *env_l)
 {
-    t_env_var *head;
-    size_t string_len;
+	t_env_var	*head;
+	size_t		string_len;
 
-    if (!env_l || !string)
-        return (NULL);
-    string_len = ft_strlen(string);
-    head = env_l;
-    while (head)
-    {
-        if (ft_strlen(head->name) == string_len && !ft_strncmp(head->name, string, string_len))
-            return (ft_strdup(head->value));
-        head = head->next;
-    }
-    return (NULL);
+	if (!env_l || !string)
+		return (NULL);
+	string_len = ft_strlen(string);
+	head = env_l;
+	while (head)
+	{
+		if (ft_strlen(head->name) == string_len
+			&& !ft_strncmp(head->name, string, string_len))
+			return (ft_strdup(head->value));
+		head = head->next;
+	}
+	return (NULL);
 }
