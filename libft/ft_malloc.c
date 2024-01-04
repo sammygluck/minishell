d117c1b6aa70c04010553d614431fad11ebe7e09
Sammy <infogls@yahoo.com>
2024-01-04 13:34:05 -0500

    major norminette overhaul

--- libft/ft_malloc.c
+++ libft/ft_malloc.c
@@ -1,14 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_malloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sgluck <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/04 13:31:26 by sgluck            #+#    #+#             */
+/*   Updated: 2024/01/04 13:31:28 by sgluck           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <libft.h>
 
-void *ft_malloc(size_t size)
+void	*ft_malloc(size_t size)
 {
-    void *new_pointer;
+	void	*new_pointer;
 
-    new_pointer = malloc(size);
-    if (!new_pointer)
-    {
-        printf("malloc failure\n");
-        exit(EXIT_FAILURE);
-    }
-    return (new_pointer);
+	new_pointer = malloc(size);
+	if (!new_pointer)
+	{
+		printf("malloc failure\n");
+		exit(EXIT_FAILURE);
+	}
+	return (new_pointer);
 }
