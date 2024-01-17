0d4e70b000ee2bef25d22e1589c1903b7f718c13
Jolande Steenput <jsteenpu@mi-r2-p5.s19.be>
2024-01-17 20:37:47 +0100

    norminette cleaning file 5e

--- 5e_env_path.c
+++ 5e_env_path.c
@@ -6,7 +6,7 @@
 /*   By: jsteenpu <jsteenpu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 13:26:24 by jsteenpu          #+#    #+#             */
-/*   Updated: 2024/01/17 18:19:28 by jsteenpu         ###   ########.fr       */
+/*   Updated: 2024/01/17 20:28:53 by jsteenpu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	**create_paths_array(char *path)
 	int		i;
 	char	**paths;
 
-	paths = ft_split(path, ':'); //make sure to free this in all cases
+	paths = ft_split(path, ':');
 	i = 0;
 	while (paths[i])
 	{
